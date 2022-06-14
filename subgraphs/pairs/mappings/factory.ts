9613ca4befe6313735c593a9ae72ba7873ae97b9
okeken <okekehinde@gmail.com>
2022-06-14 15:36:17 +0200

    chores

--- subgraphs/pairs/mappings/factory.ts
+++ subgraphs/pairs/mappings/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../generated/Factory/Factory";
 import { fetchDecimals, fetchName, fetchSymbol } from "./utils/erc20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0xB30EbAe4528a02020AC3f121952492987B1B8160";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
